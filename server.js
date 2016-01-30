d026a740c31fd1842fe35384af5b47a866d42a93
Patrick Wiese <dev@schegar.de>
2016-01-30 01:00:07 +0100

    Final static todos

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", function(req, res) {
 	res.send("Todo API Root");
 });
 
-// GET /todos
+// GET /todos?completed=true?q=house
 app.get("/todos", function (req, res){
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -22,6 +22,13 @@ app.get("/todos", function (req, res){
 		filteredTodos = _.where(filteredTodos, {"completed": true});
 	} else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
 		filteredTodos = _.where(filteredTodos, {"completed": false});
+	}
+
+	if (queryParams.hasOwnProperty("q") && queryParams.q.trim().length > 0) {
+		console.log("here");
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) !== -1;
+		})
 	}
 
 	res.send(filteredTodos);
---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name : "Patrick",
+// 	age: 19
+// }
+
+// function updatePerson(obj) {
+// 	obj.age = 18;
+// }
+
+// updatePerson(person);
+
+// console.log(person);
+
+// Array
+
+var grades = [15, 7];
+
+function addGrade(obj) {
+	obj.push(10);
+	debugger;
+}
+
+addGrade(grades);
+
+console.log(grades);
