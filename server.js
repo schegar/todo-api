5cf0a25b559109540177d13f1e02a2270f204d0d
schegar <dev@schegar.de>
2016-02-06 12:13:14 +0100

    Convert GEt /todos to database

--- server.js
+++ server.js
@@ -16,35 +16,30 @@ app.get("/", function(req, res) {
 
 // GET /todos?completed=true?q=house
 app.get("/todos", function (req, res){
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
-		filteredTodos = _.where(filteredTodos, {"completed": true});
-	} else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
-		filteredTodos = _.where(filteredTodos, {"completed": false});
+	if (query.hasOwnProperty("completed") && query.completed === "true") {
+		where.completed = true;
+	} else if (query.hasOwnProperty("completed") && query.completed === "false") {
+		where.completed = false;
+	}
+	if (query.hasOwnProperty("q") && query.q.length > 0) {
+		where.description = {
+			$like: "%" + query.q + "%"
+		};
 	}
 
-	if (queryParams.hasOwnProperty("q") && queryParams.q.trim().length > 0) {
-		console.log("here");
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) !== -1;
-		});
-	}
-
-	res.send(filteredTodos);
+	db.todo.findAll({where: where}).then(function (todos) {
+		res.send(todos);
+	}, function (e) {
+		res.status(500).send();
+	});	
 });
 
 // GET /todos/:id
 app.get("/todos/:id", function (req, res) {
 	var todoId = parseInt(req.params.id);
-	// var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	// if (matchedTodo) {		
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();
-	// }
 
 	db.todo.findById(todoId).then(function (todo) {
 		if (!!todo) {
